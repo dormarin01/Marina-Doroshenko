c89b3784ba7d6f46ae7b25468ebf5f460d2c2eee
Marina <doroshenkomarina96@gmail.com>
2018-05-24 12:48:13 +0200

    commit

---
+++ src/sweets/MyList.java
@@ -0,0 +1,12 @@
+package sweets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyList{
+public static void main(String[] args) {
+	List myList = new ArrayList();
+	myList.add(10);
+	
+}		
+}
